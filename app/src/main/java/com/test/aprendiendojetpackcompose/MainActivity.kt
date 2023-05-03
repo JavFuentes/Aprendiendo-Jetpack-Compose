77aa5d5f68703ffccc0f2b1dcc99d58100bd6322
JavFuentes <javier.antonio8128@gmail.com>
2023-05-03 02:26:47 -0400

    Implementado Scaffold y un boton flotante

--- app/src/main/java/com/test/aprendiendojetpackcompose/MainActivity.kt
+++ app/src/main/java/com/test/aprendiendojetpackcompose/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.test.aprendiendojetpackcompose
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Toast
+import android.widget.Toolbar
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -14,10 +17,15 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -26,6 +34,8 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -33,19 +43,50 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.test.aprendiendojetpackcompose.ui.theme.AprendiendoJetpackComposeTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            app()
+            ViewContainer()
         }
     }
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@ExperimentalMaterial3Api
+@Composable
+@Preview
+fun ViewContainer() {
+    Scaffold(
+        content = { Content() },
+        floatingActionButton = { FAB() },
+        floatingActionButtonPosition = FabPosition.End
+
+    )
+}
+
 @Preview
 @Composable
-fun app() {
-    var likesCounter by rememberSaveable{ mutableStateOf(0)}
+fun FAB(){
+    val context = LocalContext.current
+    FloatingActionButton(onClick = { Toast.makeText(context,"Botón operativo!", Toast.LENGTH_LONG).show() }) {
+        Text(text = "")
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun Toolbar() {
+    TopAppBar(
+        title = { Text(text = "LAPE") }, Modifier.background(color = Color.Black)
+    )
+}
+
+@Composable
+fun Content() {
+    var likesCounter by rememberSaveable { mutableStateOf(0) }
 
     LazyColumn(
         modifier = Modifier
@@ -115,7 +156,6 @@ fun app() {
                 Text(text = "Ancho:", color = Color.White)
                 Text(text = "1 m", color = Color.White)
             }
-
         }
     }
 }
